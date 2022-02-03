7b194e57c3728ab3753a5b8cd44584b0ba4798c0
Elena Popova <elipopovadev@gmail.com>
2022-02-03 21:37:03 +0200

    next version

--- List-Advanced/next_version.py
+++ List-Advanced/next_version.py
@@ -1,13 +1,4 @@
-text = input().split(".")
-numbers = list(map(int, text))
-if numbers[2] < 9:
-    numbers[2] += 1
-elif numbers[2] == 9:
-    numbers[2] = 0
-    if numbers[1] < 9:
-       numbers[1] += 1
-    else:
-        numbers[1] = 0
-        numbers[0] += 1
-
-print(*numbers, sep = '.')
+numbers = input().split(".")
+current_version = "".join(numbers)
+next_version = str(int(current_version) + 1)
+print(*next_version, sep = '.')
