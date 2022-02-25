e48463e80f45749aec622d3a3a92174b6f3d1c68
Elena Popova <elipopovadev@gmail.com>
2022-02-25 21:03:55 +0200

    bonus scoring system

---
+++ Mid Exam/05.programming_fundamentals_mid_exam/1.bonus_scoring_system.py
@@ -0,0 +1,14 @@
+number_students = int(input())
+total_count_lectures = int(input())
+additional_bonus = int(input())
+
+bonus_student_attendances = {}
+for i in range(0, number_students):
+    student_attendances = int(input())
+    bonus = round(student_attendances / total_count_lectures * (5 + additional_bonus))
+    bonus_student_attendances[bonus] = student_attendances
+    
+max_bonus = max(bonus_student_attendances.keys())
+lectures_for_student = str(bonus_student_attendances[max_bonus])
+print(f'Max Bonus: {max_bonus}.')
+print(f'The student has attended {lectures_for_student} lectures.')
